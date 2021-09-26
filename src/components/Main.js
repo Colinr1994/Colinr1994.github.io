6dcd1ac5523b8c55c00b64404368c2ffce12d92b
Colin <colinr1994@gmail.com>
2021-09-25 23:53:00 -0700

    added preliminary test

--- src/components/Main.js
+++ src/components/Main.js
@@ -34,24 +34,20 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+            Outcome driven, I have worked on software and business 
+            projects in the United States, the Philippines, and 
+            Vietnam. I enjoy the challenge of a fast paced work 
+            environment, but also firmly believe that the meaning 
+            derived from work comes from interacting with those who 
+            the work impacts. 
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+            I am looking to challenge myself by moving into work 
+            that takes advantage of my understanding of the business 
+            as a whole, and not only the fine details that make up the 
+            tech stack. I take great enjoyment from my work, but am not 
+            defined by it. Happy to get to know others, so please feel 
+            free to reach out. 
           </p>
           {close}
         </article>
@@ -64,27 +60,42 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Work</h2>
+          <p>
+            I was pleased to take part as project manager of the 
+            NCR point of sale rollout for KFC Vietnam. I was able 
+            to assist with communication between the different business 
+            partners, while also actively taking part in the software 
+            and hardware setup. Pictured below, the first KFC to receive 
+            the NCR system was a resounding success, and was a 48 hour 
+            span I won’t soon forget. 
+          </p>
           <span className="image main">
             <img src={kfc_vn_rollout} alt="" />
           </span>
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
+            Throughout my youth, and while finding my place after returning 
+            from Vietnam (during the beginning of the COVID-19 pandemic) I 
+            returned to my roots working as a farrier. This was a pivotal 
+            time for me which allowed me to solidify my desires to move forward 
+            in technology and development.
           </p>
           <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+          I am looking to challenge myself by moving into work 
+          that takes advantage of my understanding of the business 
+          as a whole, and not only the fine details that make up the 
+          tech stack. I take great enjoyment from my work, but am not 
+          defined by it. Happy to get to know others, so please feel 
+          free to reach out. 
+
           </p>
           <span className="image main">
             <img src={farrierPic} alt="" />
           </span>
+          <p>
+            I find many of the lessons learned from working with horses 
+            to transfer well to tech and business, and wouldn’t change 
+            this segment of my life for the world. 
+          </p>
           {close}
         </article>
 
@@ -100,13 +111,11 @@ class Main extends React.Component {
             <img src={pic03} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            I take great pleasure in my hobbies (of which there are many). 
+            When I’m not reading or riding motorcycles, I enjoy snowboarding, 
+            fishing, and traveling. While several of these have been tampered 
+            down by the current pandemic, I look forward to resuming activities 
+            once we are able to return to normal. 
           </p>
           {close}
         </article>
@@ -119,44 +128,26 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
+          <p>
+            Please connect with me through Linkedin, 
+            <a href = "mailto: colin@colinreed.me"> or email.</a>
+          </p>
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/ColinR1994"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
+              <a href="https://www.facebook.com/colin.reed.75873/" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
+              <a href="https://www.instagram.com/colinr1994/" className="icon fa-instagram">
                 <span className="label">Instagram</span>
               </a>
             </li>
